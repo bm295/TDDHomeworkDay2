99ef330d854e887afea03cf2dc9cfbed76a412dd
bm295 <baominh.nguyen.295@gmail.com>
2020-05-01 00:08:36 +0700

    Add FileIO

---
+++ BookStore/FileIO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BookStore
+{
+    public class FileIO
+    {
+        public string ReadSpecificLine(string filePath, int lineNumber)
+        {
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                string content = null;
+                for (int i = 1; i <= lineNumber; i++)
+                {
+                    file.ReadLine();
+                    if (file.EndOfStream)
+                    {
+                        break;
+                    }
+                }
+                content = file.ReadLine();
+
+                return content;
+            }
+        }
+    }
+}
